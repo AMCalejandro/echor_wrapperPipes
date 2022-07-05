9ca2be8a196950290fac87a21d253cafc3f19761
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2022-07-05 08:25:07 +0000

    Improving finemapping workflow

--- WORKFLOW/FINEMAPPING.R
+++ WORKFLOW/FINEMAPPING.R
@@ -6,10 +6,9 @@
 # This script takes a txt file with the following informations
 # RS_PATH
 # SS_path
-
-
 ## Example usage
 #Rscript FINEMAPPING.R [path_to_metadata_file.txt]
+
 
 
 # Activate echoR conda env
@@ -24,62 +23,75 @@ library(tools) # To perform format changes in the SS file
 library(rlang) # Data masking and more
 library(vautils) # Use util to find nearest gene from an input SNP
 library(here) # Efficient path management within the R project
-
 # Read file with metadata to perform finemapping
+library(colochelpR, lib.loc = "/mnt/rreal/RDS/acarrasco/R_libs")
+library(SNPlocs.Hsapiens.dbSNP144.GRCh37, lib.loc = "/mnt/rreal/RDS/acarrasco/R_libs")
+
+
 args <- commandArgs(trailingOnly = TRUE)
-metadata = fread(args[1])
+metadata = fread(args[1], header = F)
+metadata = as.vector(metadata$V1)
+
+
 source(here::here("R", "utils.R"))
 
-# Create / Localice RS dir
-make_results_dir(metadata[2])
 
+# This script should take the data harmonised. Perform harmonization outside this script
+fullSS_path = metadata[1]
+fullRS_path = metadata[2]
+newSS_name = metadata[3]
 
+# Create RS if not exist
+make_results_dir(fullRS_path)
 
-# Noramlly from METAL software the SNP name is MarkerName. Rename
-data = fread(metadata[1])
-if("MarkerName" %in% colnames(data)) {
-  data = data %>% rename(SNP = MarkerName) %>%
+# Read GWAS and do some wrangling
+data = fread(fullSS_path)
+if ("MarkerName" %in% colnames(data)) {
+  data = data %>% dplyr::rename(SNP = MarkerName, Pval = `P-value`) %>%
     select(SNP, everything())
-  fwrite(data, metadata[1], row.names = F, col.names = T, sep ="\t", quote = F)
+  
+  ### Data wrangling
+  # I am assuming for now that the input data has rsids
+  # This will need some extra functions as I receive new data from users
+  dbSNP = SNPlocs.Hsapiens.dbSNP144.GRCh37
+  data =  colochelpR::convert_rs_to_loc(df = data, SNP_column = "SNP", dbSNP = dbSNP) %>%
+    separate(loc, into = c("CHR", "POS")) %>%
+    mutate(CHR = as.numeric(as.character(CHR)),
+           POS = as.numeric(POS))
+  
+  # Perform some processing 
+  # We get CHRPOS for each SNP, and perform some further renaming
+  # Run here the SS_CHECK FUNCTION 
+  metadata = SS_check(data, metadata)
+} else {
+  metadata = SS_check(data, metadata)
 }
 
+# Update metada file. 
+metadata = c(metadata[2], metadata[4])
 
-# Perform minor processing in the input df
-SS_check(metadata[1])
-
-# Get the input data, take the lead variant on each (nominal) significant 
-# locus, then we find the nearest gene to it, and write out the top_SNPs file to be used in echolocatoR
-lead_variants = gwas_lead_snps(metadata[1])
-
-# Then we make the top_SNPs file
-get_topSNPs(lead_variants, build = "hg19")
-
-
-
+# Get the top_SNPs file
+data_qc = fread(metadata[2])
+lead_variants = gwas_lead_snps(data_qc)
+top_SNPs = make_topSNPs(lead_variants, build = "hg19", 
+             write.out = T, 
+             .metadata_file = metadata,
+             custom_gene = "ACP6")
 
 
 ## At this point
-
 # We have the SS GWAS updates if needed, RS updated if needed, top_SNPs file created
-
 # We are ready to run finemap_loci
+source(here::here("R", "finemap_func.R"))
+finemapping_wrapper(top_SNPs = top_SNPs,
+                    study_name = "PD_GWAS",
+                    study_type = "motor_progression",
+                    build = "hg19",
+                    finemap_tools = c("ABF", "FINEMAP", "SUSIE", "POLYFUN_SISE"),
+                    mean_SS = 3500)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-# Get SS data
-# Get SNP_GENE dat ato analyse
-# Run echolocatoR
